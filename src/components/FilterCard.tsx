7ce25601f280c16c0846bfec9adef30b18e4d18e
Nicklas Hansson <nicke_h90@hotmail.com>
2022-05-25 23:30:16 +0200

    Adding tab system for filter card

--- src/components/FilterCard.tsx
+++ src/components/FilterCard.tsx
@@ -1,15 +1,61 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+import React, { useState} from 'react'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
 
 export const FilterCard: React.FC<any> = (props) => {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  interface TabPanelProps {
+    children?: React.ReactNode
+    index: number
+    value: number
+  }
+
+  const TabPanel = (props: TabPanelProps) => {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+
   return (
     <Card sx={{ minWidth: 275 }} raised>
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Filter content
-        </Typography>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs value={value} onChange={handleChange}>
+            <Tab label="Users" />
+            <Tab label="Integrations" />
+          </Tabs>
+        </Box>
+
+        <TabPanel value={value} index={0}>
+          Users
+        </TabPanel>
+        
+        <TabPanel value={value} index={1}>
+          Integrations
+        </TabPanel>
       </CardContent>
     </Card>
   )
