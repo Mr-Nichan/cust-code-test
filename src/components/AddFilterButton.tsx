df8e518ca3beed30ce9028739a2c6eb7d205546b
Nicklas Hansson <nicke_h90@hotmail.com>
2022-05-26 11:49:07 +0200

    Adding styling to components

--- src/components/AddFilterButton.tsx
+++ src/components/AddFilterButton.tsx
@@ -1,6 +1,20 @@
 import React from 'react'
+import { styled } from '@mui/system'
 import { Button } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add'
+
+const StyledButton = styled(Button, {})({
+  maxWidth: '128px',
+  fontFamily: 'Work Sans',
+  fontSize: '14px',
+  lineHeight: '16px',
+  height: '40px',
+  borderRadius: '5px',
+  border: '1.2px solid #339933',
+  textTransform: 'none',
+  color: '#65B366',
+  boxSizing: 'border-box'
+})
 
 interface IButtonProps {
   name: any
@@ -8,12 +22,12 @@ interface IButtonProps {
 
 export const AddFilterButton: React.FC<IButtonProps> = ({name}: IButtonProps) => {
     return (
-        <Button
+        <StyledButton
           onClick={() => console.log('Boop!')}
           variant="outlined"
           startIcon={<AddIcon />}
           >
             {name}
-          </Button>
+          </StyledButton>
     )
 }
--- src/components/FilterCard.tsx
+++ src/components/FilterCard.tsx
@@ -1,10 +1,74 @@
 import React, { useState} from 'react'
+import { styled } from '@mui/system'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
+
+const StyledCard = styled(Card, {})({
+  maxWidth: '340px',
+  borderRadius: '5px',
+  border: '1px solid #F8F8F8',
+  boxShadow: '0px 15px 35px rgba(0, 0, 0, 0.1)'
+})
+
+const StyledCardContent = styled(CardContent, {})({
+  padding: 0
+})
+
+const StyledTab = styled((props: StyledTabProps) => <Tab disableRipple {...props} />)(
+  ({ theme }) => ({
+    textTransform: 'none',
+    minWidth: 0,
+    marginRight: theme.spacing(1),
+    color: 'rgba(0, 0, 0, 0.85)',
+    fontFamily: 'Work Sans',
+    '&:hover': {
+      color: '#65B366',
+      opacity: 1,
+    },
+    '&.Mui-selected': {
+      color: '#339933',
+      fontWeight: '600'
+    },
+    '&.Mui-focusVisible': {
+      backgroundColor: '#d1eaff',
+    },
+  }),
+);
+
+interface StyledTabProps {
+  label: string;
+}
+
+const StyledTabs = styled((props: StyledTabsProps) => (
+  <Tabs
+    {...props}
+    TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
+  />
+))({
+  '& .MuiTabs-indicator': {
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+  },
+  '& .MuiTabs-indicatorSpan': {
+
+    top: '2px',
+    borderBottom: '1.3px solid #339933',
+    maxWidth: '70%',
+    width: '100%',
+    // backgroundColor: '#339933',
+  },
+});
+
+interface StyledTabsProps {
+  children?: React.ReactNode;
+  value: number;
+  onChange: (event: React.SyntheticEvent, newValue: number) => void;
+}
 
 export const FilterCard: React.FC<any> = (props) => {
   const [value, setValue] = useState(0);
@@ -26,12 +90,10 @@ export const FilterCard: React.FC<any> = (props) => {
       <div
         role="tabpanel"
         hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
         {...other}
       >
         {value === index && (
-          <Box sx={{ p: 3 }}>
+          <Box sx={{ p: 2 }}>
             <Typography>{children}</Typography>
           </Box>
         )}
@@ -40,23 +102,26 @@ export const FilterCard: React.FC<any> = (props) => {
   }
 
   return (
-    <Card sx={{ minWidth: 275 }} raised>
-      <CardContent>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <Tabs value={value} onChange={handleChange}>
-            <Tab label="Users" />
-            <Tab label="Integrations" />
-          </Tabs>
+    <StyledCard sx={{ minWidth: 275 }} raised>
+      <StyledCardContent>
+        <Box sx={{ boxSizing: 'border-box', borderBottom: '1.3px solid #eee' }}>
+          <StyledTabs
+            value={value}
+            onChange={handleChange}
+          >
+            <StyledTab label="Users" />
+            <StyledTab label="Integrations" />
+          </StyledTabs>
         </Box>
 
         <TabPanel value={value} index={0}>
           Users
         </TabPanel>
-        
+
         <TabPanel value={value} index={1}>
           Integrations
         </TabPanel>
-      </CardContent>
-    </Card>
+      </StyledCardContent>
+    </StyledCard>
   )
 }
