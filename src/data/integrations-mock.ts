668837cb1a697d6bf0c1aafcab69fd424c7734d9
Nicklas Hansson <nicke_h90@hotmail.com>
2022-05-26 16:17:01 +0200

    Adding mock data

---
+++ src/data/integrations-mock.ts
@@ -0,0 +1,27 @@
+export const integrations = [
+  {
+    id: 1,
+    image: null,
+    name: 'ActiveCampaign'
+  },
+  {
+    id: 2,
+    image: null,
+    name: 'Google Analytics'
+  },
+  {
+    id: 3,
+    image: null,
+    name: 'Instagram'
+  },
+  {
+    id: 4,
+    image: null,
+    name: 'Woocommerce'
+  },
+  {
+    id: 5,
+    image: null,
+    name: 'Shopify'
+  }
+]
---
+++ src/data/users-mock.ts
@@ -0,0 +1,32 @@
+export const users = [
+  {
+    id: 1,
+    image: null,
+    name: 'Louie Popp'
+  },
+  {
+    id: 2,
+    image: null,
+    name: 'Nicklas Herrera Hansson'
+  },
+  {
+    id: 3,
+    image: null,
+    name: 'Jonas Rafn'
+  },
+  {
+    id: 4,
+    image: null,
+    name: 'Fiona Rakipi'
+  },
+  {
+    id: 5,
+    image: null,
+    name: 'Martin Navne'
+  },
+  {
+    id: 6,
+    image: null,
+    name: 'Kristoffer Degn'
+  },
+]
